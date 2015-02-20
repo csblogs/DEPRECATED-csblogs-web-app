d7208696accde52089dc174f5bc6fee7c8e79cb8
Rob Crocombe <rjwc1993@yahoo.co.uk>
2015-02-20 22:52:40 +0000

    Added less package

--- server.js
+++ server.js
@@ -1,30 +1,36 @@
 var express = require('express')
 var compression = require('compression')
 var exphbs  = require('express-handlebars')
-var database = require('./database.js')
+//var database = require('./database.js')
 
 var app = express()
 app.use(compression())
 app.use(express.static(__dirname + '/static'))
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+app.use(require('less-middleware')(
+    __dirname + 'static/style/less', // source
+    { dest: __dirname + 'static/style/css' }, // options
+    {}, // parser
+    { compress: 'auto' } // complier
+))
 
-// respond with "hello world" when a GET request is made to the homepage
+// respond with the handlebars compiled homepage
 app.get('/', function(req, res) {
   res.render('index', {title:"Index | CS Blogs"})
 })
 
 // respond with bloggers from mongo
-app.get('/bloggers',function(req, res) {
-        database.Blogger.find(function(error,bloggers) {
-        if(error) {
-            console.log('Error fetching bloggers')
-        }
-        else {
-            res.render('bloggers', {title:"All Bloggers | CS Blogs", bloggers: bloggers})
-        }
-    })
-})
+//app.get('/bloggers',function(req, res) {
+//        database.Blogger.find(function(error,bloggers) {
+//        if(error) {
+//            console.log('Error fetching bloggers')
+//        }
+//        else {
+//            res.render('bloggers', {title:"All Bloggers | CS Blogs", bloggers: bloggers})
+//        }
+//    })
+//})
 
 var port = process.env.PORT || 3000; //process.evn.PORT is required to work on Azure
 var server = app.listen(port, function () {
